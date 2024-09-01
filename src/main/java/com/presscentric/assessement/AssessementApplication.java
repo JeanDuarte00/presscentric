2d1df05a0abea70a4b0aadad8480f3bc70a62beb
Jean Duarte <jean_luccas320@hotmail.com>
2024-09-01 19:02:35 -0300

    $ feat: first commit

---
+++ src/main/java/com/presscentric/assessement/AssessementApplication.java
@@ -0,0 +1,13 @@
+package com.presscentric.assessement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessementApplication.class, args);
+	}
+
+}
